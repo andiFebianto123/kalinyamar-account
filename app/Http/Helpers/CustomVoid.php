00d3ad495413e8303b54ad1f72422c79e7c76d83
andiFebianto123 <andifebianto123@gmail.com>
2026-04-25 12:12:27 +0700

    void store transaksi invoice

--- app/Http/Helpers/CustomVoid.php
+++ app/Http/Helpers/CustomVoid.php
@@ -859,7 +859,7 @@ class CustomVoid
 
             if ($invoice->withholding_agent == InvoiceClient::WITHHOLDING_AGENT['WAPU']) {
                 $nominal_transfer_account = $nominal_transaction - $invoice->discount_pph;
-            } else if ($invoice->witholding_agent == InvoiceClient::WITHHOLDING_AGENT['NON_WAPU']) {
+            } else if ($invoice->withholding_agent == InvoiceClient::WITHHOLDING_AGENT['NON_WAPU']) {
                 $price_ppn = $nominal_transaction * ($invoice->tax_ppn / 100);
                 $pirce_pph = $invoice->discount_pph;
                 $nominal_transfer_account = $nominal_transaction + $price_ppn - $pirce_pph;
